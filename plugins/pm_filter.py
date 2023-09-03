d715c470ab4676c161d238fa3f7bc21bd632c154
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-09-03 10:26:54 +0530

    adding user notification message for request

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -655,6 +655,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
                 await client.send_message(req_channel, f"#Requested_logs \n\n**Content Name:**`{search}` \n**Requested By:** {message.from_user.first_name}\n**User ID:** {message.from_user.id}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Mark as done", callback_data="close_data")]]))
+                await message.reply_text(text=f"Hello `{message.from_user.first_name}` 😇. \n\n Your Request has been sent to our admin ! \n\n ❣ Thank You ❣ <a href='https://t.meCodeNexus_community'>Channel Name<a/> ")
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
